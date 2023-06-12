2abbc3df8bace337734e726daf21fbd671f46f99
Ju4n Avendanoa <juanfernando.avendanoa@gmail.com>
2023-06-12 17:49:27 -0500

    adding app and index files

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import express, { json } from "express";
+import express from "express";
 import morgan from "morgan";
 import productRouter from "./routes/products.routes.js";
 import cors from "cors";
